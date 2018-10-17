73b4d34995e076f5f02f979add2fdadbef3db27b
Jorge <jbocanegragrandez@gmail.com>
2018-10-17 10:53:23 -0700

    fix reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
-import businessesReducer from './businesses_Reducer';
+import businessesReducer from './businesses_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
