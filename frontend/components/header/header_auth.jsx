9f0d548bacbe38bd76d80c5fbf1ba71d5b210273
Jorge <jbocanegragrandez@gmail.com>
2018-11-25 21:49:29 -0800

    add styling and new logo

--- frontend/components/header/header_auth.jsx
+++ frontend/components/header/header_auth.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const headerauth = () => (
   <div className="page-header">
+    <Link to="/" className="header-link">
+    <div className="logo-background-mini"></div>
+    </Link>
   </div>
 );
 
