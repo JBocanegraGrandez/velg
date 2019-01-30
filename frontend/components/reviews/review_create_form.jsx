f5dcb1b98d895efcfc11ef9e63bdeb422aa39b2f
Jorge <jbocanegragrandez@gmail.com>
2019-01-30 15:35:21 -0800

    add google analytics script

---
+++ frontend/components/reviews/review_create_form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class ReviewForm extends React.Component {
+    constructor(props){
+        super(props);
+    }
+}
+
+render () {
+    
+}
